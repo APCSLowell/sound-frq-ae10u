003330ca290544c0de8ec0e3379aa8b1304eb87b
ae10u <142628398+ae10u@users.noreply.github.com>
2024-04-15 23:08:06 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -16,14 +16,15 @@ public class Sound
   public int limitAmplitude(int limit)
   {  
    int numChanged = 0;
-   for (int i = 0; i < samples.length; i++) {
-   if (samples[i] < -limit) {
-   samples[i] = -limit;
-   numChanged++;
+   for (int i = 0; i < samples.length; i++) 
+   {
+     if (samples[i] > limit) {
+       samples[i] = limit;
+       numChanged++;
    }
-   if (samples[i] > limit) {
-   samples[i] = limit;
-   numChanged++;
+   if (samples[i] < -limit) {
+     samples[i] = -limit;
+     numChanged++;
    }
    }
    return numChanged;
